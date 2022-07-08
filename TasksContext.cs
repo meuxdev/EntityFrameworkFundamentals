1b184448eb6d15ecade8663b803fbec1121db7a4
Alejandro Andrade <meuxdev@gmail.com>
2022-07-08 14:43:29 -0500

    Adding: Task Context.cs

---
+++ TasksContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using projectef.Models;
+
+namespace projectef;
+
+// Database context configuration
+public class TasksContext : DbContext
+{
+    public DbSet<Category> Categories { get; set; }
+
+    public DbSet<TodoTask> TodoTasks { get; set; }
+
+    public TasksContext(DbContextOptions<TasksContext> options) : base(options) { }
+}
