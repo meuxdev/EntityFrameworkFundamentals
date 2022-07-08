ae769455228ae4f0953b7f95a5ff4fe9cdec7ad5
Alejandro Andrade <meuxdev@gmail.com>
2022-07-08 15:31:25 -0500

    Database in memory usage

--- Program.cs
+++ Program.cs
@@ -1,6 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using projectef;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Adding the context
+// Basic EF configuration
+builder.Services.AddDbContext<TasksContext>(opt => opt.UseInMemoryDatabase("TasksDB"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/dbconnection", async ([FromServices] TasksContext dbContext) =>
+{
+    dbContext.Database.EnsureCreated();
+    return Results.Ok($"DB in memory successfully: {dbContext.Database.IsInMemory()}");
+});
 
 app.Run();
